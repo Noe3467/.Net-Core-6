c1290fa90dc84967512533f5fca185abd98ab26f
Noelia Romina Ruiz <92758405+Noe3467@users.noreply.github.com>
2022-05-17 06:11:32 -0300

    ejercicio de la clase 5 en c#

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿
+//bucle do while
+string respuesta; //no necesito inicializarla
+do
+{
+    Console.Clear(); //paso (1) - limpiar la pantalla
+
+    Console.WriteLine("ingresá tu nombre: "); //paso (2) - pedir el nombre
+    var nombre = Console.ReadLine();
+
+    Console.WriteLine($"Hola {nombre}!"); //paso (3) - saludarlo
+
+    Console.WriteLine("¿Deseas continuar? INGRESÁ--> 'S' para sí, 'N' para no <--"); //paso (4) - preguntar
+    respuesta = Console.ReadLine();
+}
+while (respuesta.ToLower() == "s");
+
+
+if (respuesta.ToUpper() == "N")
+{
+    Console.WriteLine("programa finalizado correctamente.");
+}
+else
+{
+    Console.WriteLine("Opción no valida, ingresá nuevamente.");
+}
